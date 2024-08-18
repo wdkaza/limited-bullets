2758c350b401b72e34b59df46a31fa996d5bcf51
wdkaza <b.g.kirienko@gmail.com>
2024-08-18 20:50:35 +0300

    Update main.cpp
    you can now play it

--- main.cpp
+++ main.cpp
@@ -6,12 +6,24 @@ Color DarkGreen = Color{20,160,133,255};
 Color Light_Grey = Color{38,185,154,255};
 Color Yellow = Color{243,213,91,255};
 
-#include <raylib.h>
-
 class Ammo {
 public:
     float x, y;
+    float width = 50;
+    float height = 50;
 
+
+    Ammo()
+    {
+        Vector2 position = GenRanPos();
+        x = position.x;
+        y = position.y;
+    }
+    Vector2 GenRanPos()
+    {
+        Vector2 position = GenerateRandomPos();
+        return position;
+    }
     // Texture2D texture;
     // Ammo()
     // {
@@ -25,18 +37,18 @@ public:
     // }
 
     void draw() {
-        Vector2 position = GenerateRandomPos();
-        // DrawTexture(texture, position.x, position.y,WHITE);
-        DrawRectangle(position.x,position.y,50, 50, RED);
+        DrawRectangle(x,y,50, 50, RED);
     }
 
     Vector2 GenerateRandomPos() {
         x = GetRandomValue(200, 1000);
         y = GetRandomValue(100, 700);
         return Vector2{x, y};
-    }
 };
-
+    Rectangle GetRect() {
+        return Rectangle{x, y, width, height};
+    }
+    };
 class Player {
 public:
     float pos_x, pos_y;
@@ -45,6 +57,9 @@ public:
     float speed_x, speed_y;
     float bounce_strength;
     int bullets;
+    Rectangle GetRect();
+    void drawHitbox(bool isColliding);
+    void eat(bool isColliding);
 
     void Draw() {
         DrawCircle(pos_x, pos_y, radius, RED);
@@ -52,33 +67,27 @@ public:
 
     void Update(Vector2 mousePosition) {
         if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-            // Calculate the direction vector from the ball to the mouse
             float dir_x = mousePosition.x - pos_x;
             float dir_y = mousePosition.y - pos_y;
 
-            // Normalize the direction vector
             float length = sqrt(dir_x * dir_x + dir_y * dir_y);
             dir_x /= length;
             dir_y /= length;
 
-            // Move the ball in the opposite direction of the mouse
             speed_x = -dir_x * bounce_strength;
             speed_y = -dir_y * bounce_strength;
             pos_x += speed_x;
             pos_y += speed_y;
             bullets -= 1;
         } else {
-            // Apply gravitational force to the left and right
             if (pos_x < GetScreenWidth() / 2) {
-                speed_x += accel_x;  // Gravity pulls to the right
+                speed_x += accel_x; 
             } else {
-                speed_x -= accel_x;  // Gravity pulls to the left
+                speed_x -= accel_x; 
             }
-
-            // Apply vertical gravity
+            
             speed_y += accel_y;
 
-            // Update position
             pos_x += speed_x;
             pos_y += speed_y;
         }
@@ -102,17 +111,20 @@ int main() {
     player.accel_x = 0.1;
     player.accel_y = 0.3;
     player.bounce_strength = 15;
-    player.bullets = 10;
+    player.bullets = 5;
 
-    while (WindowShouldClose() == false) {
+    while (WindowShouldClose() == false && player.bullets >= 0) {
         BeginDrawing();
         ClearBackground(DarkGreen);
+        bool isColliding = CheckCollisionCircleRec(Vector2{player.pos_x, player.pos_y}, player.radius, ammo.GetRect());
 
         Vector2 mousePosition = GetMousePosition();
         DrawCircle(screen_width/2, screen_height/2, 150, Light_Grey);
         DrawText(TextFormat("%i", player.bullets), screen_width/2 -30, screen_height/2 -50, 100, DARKGRAY);
         player.Update(mousePosition);
         player.Draw();
+        player.drawHitbox(isColliding);
+        player.eat(isColliding);
         DrawLine(player.pos_x, player.pos_y, mousePosition.x, mousePosition.y, Yellow);
         ammo.draw();
 
@@ -122,4 +134,22 @@ int main() {
 
     CloseWindow();
     return 0;
+}
+
+Rectangle Player::GetRect() {
+    return Rectangle{pos_x - radius, pos_y - radius, radius * 2, radius * 2};
+}
+
+void Player::drawHitbox(bool isColliding)
+{
+    Color outlinecolor = isColliding ? RED : BLACK;
+    DrawRectangleLinesEx(GetRect(),3,outlinecolor);
+}
+void Player::eat(bool isCollding)
+{
+    if (isCollding == true)
+    {
+        ammo.GenRanPos();
+        player.bullets++;
+    }
 }
